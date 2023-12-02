a7b6bc871106256da9e266df3183022c21fc3ad3
jerecoder <jerefigueiredo@gmail.com>
2023-12-02 11:23:04 -0500

    no se

--- ej1.py
+++ ej1.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 # Tomando n = 5 y d = 100 genere matrices A y vectores b aleatorios y resuelva el problema minimizando
 # F y F2 . Tome δ_2 = 10−2 σmax cuando trabaje con F2 . En todos los casos utilice s = 1/λmax ,
@@ -37,35 +38,81 @@ def max_eigenvector(A):
     vals, _  = np.linalg.eig(A)
     return vals[0]
 
-def gradient_descent(A, b, func, gradient,  iter = int(1e3)):
+def gradient_descent(A, b, func, gradient,  iter = int(1e3), step_size = None):
     x = np.random.uniform(size=A.shape[1])
-    step_size = 1 / max_eigenvector(H(A))
+    if(step_size==None):
+        step_size = 1 / max_eigenvector(H(A))
     cost = func(A, b, x)
     norms, costs = [],[]
     for _ in range(iter):
         x = x - step_size*gradient(A, b, x)
-        costs.append(func(A, b, x))
+        cost = func(A, b, x)
+        costs.append(cost)
         norms.append(np.linalg.norm(x, ord = 2))
-        cost = F(A, b, x)
-    return (np.linalg.norm(x, ord = 2), cost, norms, costs)
+        
+    return (np.linalg.norm(x, ord = 2), F(A, b, x), norms, costs, x)
+
+def gradient_descent_momentum(A, b, func, gradient, beta=0.9, iter=int(1e3)):
+    x = np.random.uniform(size=A.shape[1])
+    velocity = np.zeros(A.shape[1])
+    step_size = 1 / max_eigenvector(H(A))
+    costs = []
+
+    for _ in range(iter):
+        grad = gradient(A, b, x)
+        velocity = beta * velocity + (1 - beta) * grad
+        x -= step_size * velocity
+        costs.append(func(A, b, x))
+        
+    return x, costs
+
+def solve_svd(A, b):
+    U, S, VT = np.linalg.svd(A)
+    S_inv = np.zeros((A.shape[1], A.shape[0]))
+    S_inv[:len(S), :len(S)] = np.diag(1 / S)
+    A_pseudo_inverse = VT.T @ S_inv @ U.T
+    x = A_pseudo_inverse @ b
+    return x
+
 
 def main():
-    iterations, n, d = 1, 5, 100
+    iteraciones, n, d = 1, 5, 100
     norm_sum_F, norm_sum_F_2, norm_sum_SVD = 0, 0, 0
-    for _ in range(iterations):
+    SOL = np.zeros(shape=10)
+    for _ in range(iteraciones):
         random_A, random_b = generate_random(n, d)
-        X1, X2 = gradient_descent(random_A, random_b, F, F_gradient), gradient_descent(random_A, random_b, F_2, F_2_gradient)
-        plt.plot(X1[2], label="x conseguido con F_1")
-        plt.plot(X2[2], label="x conseguido con F_2")
-        plt.xlabel("iteraciones")
-        plt.ylabel("norma L2")
-        plt.title("Norma L2 del vector solucion en base a la cantidad de iteraciones")
+        X1, X2, X_SVD, X1_Momentum, X2_Momentum = gradient_descent(random_A, random_b, F, F_gradient), gradient_descent(random_A, random_b, F_2, F_2_gradient),solve_svd(random_A, random_b), gradient_descent_momentum(random_A, random_b, F, F_gradient), gradient_descent_momentum(random_A, random_b, F_2, F_2_gradient)
+        #step size experiment
+        costs, step_sizes = [], []
+        original_step_size = 1 / max_eigenvector(H(random_A))
+        start, end = -200*original_step_size, 200*original_step_size
+        step = (end - start)/100
+        for s in np.arange(start, end, step):
+            x1 = gradient_descent(random_A, random_b, F, F_gradient)
+            costs.append(F(random_A, random_b, x1[-1]))
+            step_sizes.append(s)
+        
+        plt.axvline(x=original_step_size, color='red', linestyle='--')
+        # Add a label to the vertical line
+        # plt.text(original_step_size, plt.ylim()[1], 'Step Size Original', rotation=90, verticalalignment='top')
+        plt.xlabel("step-size")
+        plt.ylabel("Costo")
         plt.legend()
+        plt.plot(step_sizes, costs)
         plt.show()
+        ##########PLOT cost vs iteraciones de F1 F2#########################
+        # plt.plot(X1[3], label="x conseguido con F_1")
+        # plt.plot(X2[3], label="x conseguido con F_2")
+        # plt.xlabel("iteraciones")
+        # plt.ylabel("Costo")
+        # plt.title("Costo del vector solucion en base a la cantidad de iteraciones")
+        # plt.legend()
+        # plt.show()
+        SOL += (X1[0], X1[1], X2[0] , X2[1], np.linalg.norm(X_SVD, ord=2), F(random_A, random_b, X_SVD), np.linalg.norm(X1_Momentum[-1], ord=2), X1_Momentum[1][-1], np.linalg.norm(X2_Momentum[0], ord=2), X2_Momentum[1][-1])
         norm_sum_F += X1[0]
         norm_sum_F_2 += X2[0]
-        
-    print(norm_sum_F/iterations, norm_sum_F_2/iterations)
+    SOL/=iteraciones
+    print(SOL)
 
 if __name__ == "__main__":
     main()
